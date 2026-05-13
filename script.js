7a3c0ebb2a4b697a83e4159c331854aa2c303dc3
Syed Muhammad Raza Zaidi <rsyedmuhammad428@gmail.com>
2026-05-13 15:39:49 -0700

    Deploy: Fix hardcoded API_URL fallback inside script.js

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ let filters = {
     sort: 'featured',
 };
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost'
+    ? 'http://localhost:5000/api' 
+    : '/api';
 const STORAGE_USER_KEY = 'ec_user';
 
 const qs = (s, r = document) => r.querySelector(s);
